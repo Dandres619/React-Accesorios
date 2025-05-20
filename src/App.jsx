4336437c1f36b532bde8853cd0311851afdc9d99
Dandres <danielmta619@gmail.com>
2025-05-19 22:18:47 -0500

    Actualización completa

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import { Header } from './Header/Header'
+import './App.css';
+import { Header } from './Header/Header';
+import { Footer } from './Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-      </Routes>
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+        <main className="flex-grow-1">
+          <Routes>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
