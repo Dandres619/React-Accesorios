bc566f490e6e91d5c97d8e2cf29822a3f167dec8
Dandres <danielmta619@gmail.com>
2025-05-19 21:58:40 -0500

    Header finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import { Header } from './Header/Header'
 
 function App() {
   return (
-    <Header />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,19 +1,30 @@
+import logo from '../assets/tienda-logo.png'
+import { Link } from 'react-router-dom'
+
 export function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">11deal</a>
+          <Link to="/" className="navbar-brand">
+            <img 
+            src={logo} 
+            alt="Logo de la pagina"
+            width="90"
+            height="80"
+            className="d-inline-block align-text-top" 
+            />
+          </Link>
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                <Link to="/" className="nav-link active" aria-current="page">Inicio</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Calidad</a>
+                <Link to="/calidad" className="nav-link">Calidad</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Productos</a>
+                <Link to="/productos" className="nav-link">Productos</Link>
               </li>
             </ul>
           </div>
