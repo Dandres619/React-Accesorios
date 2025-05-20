d47869c38df83a857d81275699890a125e1d2e3d
Dandres <danielmta619@gmail.com>
2025-05-19 21:00:03 -0500

    Logo del proyecto

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,7 +3,7 @@ export function Header() {
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Accesorios</a>
+          <a className="navbar-brand" href="#">11deal</a>
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
